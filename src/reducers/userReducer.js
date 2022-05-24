654e74688b9e44a3d2406a536dc94fab98a6bb9f
sharp <98083474+SharpTH@users.noreply.github.com>
2022-05-24 07:39:02 +0700

    Update userReducer.js

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,6 +1,23 @@
-const tam = "test"
+ 
+export const useShopReducer = (state = [], action) => {
+  switch (action.type) {
+    case "SET_SHOP_DATA":
+      return action.payload;
+    case "ADD_SHOP_DATA":
+      return [...state, action.payload];
+      case "DELETE_SHOP_DATA":
+      return state.filter(item => item.id !== action.payload);
+      case "UPDATE_SHOP_DATA":
+      return state.map(item => {
+        if (item.id === action.payload.id) {
+          return action.payload;
+        } else {
+          return item;
+        }
+      });
+   default:
+      return state;
+  }
 
-
-export const userReducer = (state = "test") => {
-  return state
 }
+
